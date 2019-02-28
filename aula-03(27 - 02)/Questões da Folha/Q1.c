e52e0e9b1cf5733df3671c50cc3576dcbbfb56ce
Rafael Neves <43051996+rafaelfneves@users.noreply.github.com>
2019-02-27 21:54:10 -0300

    Add files via upload

---
+++ aula-03(27 - 02)/Questões da Folha/Q1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	
+	int n1,n2,i,maior;
+	
+	printf("Digite o numero 1: ");
+	scanf("%d", &n1);
+	
+	printf("Digite o numero 2: ");
+	scanf("%d", &n2);
+	
+	if(n1>n2){
+		
+	}
+	
+	return 0;
+}
+
+int f1(int n1){
+	
+}

cf37930c206d4990bf6854f994d0f1b840d8c26d
Rafael Faustini Costa de Oliveira <contato@rafaelfaustini.com.br>
2019-04-15 14:46:32 -0300

    Alterações de desempenho e legibilidade
    * Não é necessário o '={0}' na linha 9
    * O Switch case proporciona um melhor desempenho no código, legibilidade (É mais fácil de navegar pelas as alternativas), além de tratar opções inválidas (linha 86 a 89)
    
    Obs: Seu código foi é muito bem documentado e organizado !

--- teste-modular.c
+++ teste-modular.c
@@ -6,7 +6,7 @@
 void func(int x, int opcao, int ocupada, int livre, int vaga[]);
 
 int main(){
-	int vaga[20]={0}; 
+	int vaga[20]; 
 	int  x, opcao, ocupada, livre;
 	
 	// preencher o vetor com zeros (vaga livre)
@@ -31,35 +31,33 @@ int main(){
 
 void func(int x, int opcao, int ocupada, int livre, int vaga[])
 {	
-		if(opcao == 1)
-		{
-			system("cls"); // limpa a tela
-			ocupada = 0; 
-			for(x=0;x<20;x++){
-				printf("%d", vaga[x]);
-				if(vaga[x] == 1) ocupada++;
-			}//fim-for
-			printf("\nOcupadas = %d", ocupada);
-			printf("\nLivres = %d \n", 20-ocupada);
-			system("pause"); // aguarda tecla
-		}//fim-opcao-1
-		
-		else if (opcao == 2)
-		{
-			//encontrar uma vaga
-			livre = -1;  // sem vaga livre
-			
-			for(x=0; x<20; x++){
-				if(vaga[x] == 0){
-					livre = x;
-					break; // encerra o loop for
-				}
-			}
-			
-			if(livre == -1){
-				printf("Nao ha vagas! \n");
+		switch(opcao){
+			case 1: // Opção 1
+				system("cls"); // limpa a tela
+				ocupada = 0; 
+				for(x=0;x<20;x++){
+					printf("%d", vaga[x]);
+					if(vaga[x] == 1) ocupada++;
+				}//fim-for
+				printf("\nOcupadas = %d", ocupada);
+				printf("\nLivres = %d \n", 20-ocupada);
+				system("pause"); // aguarda tecla
+				break; // Fim Opção 1
+				
+			case 2: // Opção 2
+				//encontrar uma vaga
+				livre = -1;  // sem vaga livre
+
+				for(x=0; x<20; x++){
+					if(vaga[x] == 0){
+						livre = x;
+						break; // encerra o loop for
+					}
+				} 
+				if(livre == -1){
+					printf("Nao ha vagas! \n");
 				system("pause");
-			}else{
+				}else{
 				printf("A vaga %d esta livre.\n", livre);
 				printf("Usar? (1-sim/2-nao) ");
 				scanf("%d",&x);
@@ -67,26 +65,27 @@ void func(int x, int opcao, int ocupada, int livre, int vaga[])
 					vaga[livre] = 1;
 					printf("Entrada registrada! \n");
 					system("pause");
-				}//fim-if-usar
-			}//fim-if-livre
-		}//fim-opcao-2
+					}//fim-if-usar
+				}//fim-if-livre
+				break; // Fim Opção 2
+			
+			case 3: // Opção 3
+				printf("Qual vaga deseja liberar? (0-19) ");
+				scanf("%d",&x);
+				if( x<0 || x>19){ 
+					printf("Vaga invalida! \n");
+				}else if(vaga[x] == 0){
+					printf("A vaga ja esta livre! \n");
+				}else{
+					vaga[x] = 0;
+					printf("Vaga liberada! \n");
+				}
+				system("pause");
+				break; // Fim Opção 3
 		
-		else if (opcao == 3)
-		{
-			printf("Qual vaga deseja liberar? (0-19) ");
-			scanf("%d",&x);
-			if( x<0 || x>19){ 
-				printf("Vaga invalida! \n");
-			}else if(vaga[x] == 0){
-				printf("A vaga ja esta livre! \n");
-			}else{
-				vaga[x] = 0;
-				printf("Vaga liberada! \n");
-			}
-			system("pause");
-		}//fim-opcao-3
-		
+			default:
+				printf("Opcao invalida\n");
+				system("pause"); // aguarda tecla	
+		}
 
 }
-
-
